f67db47613a0e21931067787ab114e92e8322ccb
guidovarela <guido.varela@gmail.com>
2022-10-24 16:52:57 -0300

    update home

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+
+/* Scripts del proyecto */
+
+/* Validaacion del formulario */
+(() => {
+    'use strict'
+  
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
